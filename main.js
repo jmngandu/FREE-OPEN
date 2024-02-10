268dae007c6fe443b69519da9a826ceb608375a6
jmngandu <jamesmngandu@gmail.com>
2024-02-11 01:44:39 +0300

    commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const humberger = document.querySelector(".humberger-menu")
+const navs = document.querySelector(".navs")
+const crossBar = document.querySelector(".fa-xmark")
+const lineBar = document.querySelector(".fa-bars")
+
+
+lineBar.addEventListener("click", () => {
+    navs.style.display = "block"
+    lineBar.style.display = "none"
+    crossBar.style.display = "block"
+})
+crossBar.addEventListener("click", () => {
+    navs.style.display = "none"
+    lineBar.style.display = "block"
+    crossBar.style.display = "none"
+    window.scrollTo({top: 0, behavior: "smooth"})
+})

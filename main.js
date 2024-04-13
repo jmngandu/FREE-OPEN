38c9c2f9c9ae71846650e5fd3eb2ecaf96fd415f
jmngandu <jamesmngandu@gmail.com>
2024-04-13 12:26:32 +0300

    commit

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-const humberger = document.querySelector(".humberger-menu")
-const navs = document.querySelector(".navs")
-const crossBar = document.querySelector(".fa-xmark")
-const lineBar = document.querySelector(".fa-bars")
-const loadwithme = document.querySelector(".loading-center")
+const humberger = document.querySelector(".humberger-menu");
+const navs = document.querySelector(".navs");
+const crossBar = document.querySelector(".fa-xmark");
+const lineBar = document.querySelector(".fa-bars");
+const loadwithme = document.querySelector(".loading-center");
 
 window.addEventListener("load", function () {
-    loadwithme.style.display = "none"
-})
+  loadwithme.style.display = "none";
+});
 
 lineBar.addEventListener("click", () => {
-    navs.style.display = "block"
-    lineBar.style.display = "none"
-    crossBar.style.display = "block"
-})
+  navs.style.display = "block";
+  lineBar.style.display = "none";
+  crossBar.style.display = "block";
+});
 crossBar.addEventListener("click", () => {
-    navs.style.display = "none"
-    lineBar.style.display = "block"
-    crossBar.style.display = "none"
-    window.scrollTo({top: 0, behavior: "smooth"})
-})
+  navs.style.display = "none";
+  lineBar.style.display = "block";
+  crossBar.style.display = "none";
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
